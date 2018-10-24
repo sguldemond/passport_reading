b16554f5200194161cd0c7b2a46ed8397ebfd28f
stan-is-laus <stanguldemond@gmail.com>
2018-10-24 14:25:28 +0200

    python tests

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# passport_reading
+# Passport reading
+
+## Testing scanner
+
+The scanner we're using the ACS ACR1215U-M1 scanner.
--- readerTest.py
+++ readerTest.py
@@ -5,17 +5,13 @@ from pypassport.openssl import OpenSSLException
 rm = ReaderManager()
 print(rm.getReaderList())
 
-# rm.waitForCard()
-
-reader = rm.getReaderList()[1]
-rm.create(reader)
-
-print(type(reader))
+reader = rm.waitForCard()
 
 print(reader)
 
 # mrz = mrz.MRZ('NXHF07F75ONLD9109032M2510262207074392<<<<<68')
-# print(mrz.checkMRZ())
+# print(mrz.checkMRZ()) 
 
 # p = EPassport(mrz, reader)
+
 # print(p["DG1"])
